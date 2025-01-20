20d4fa1cd9b9089d8826214a84d372b5ea7ab6a0
Giulia0515 <geegeecucci15@gmail.com>
2025-01-20 17:59:11 -0500

    Implemented my code for all behviours. Needs some adjustments

--- Assignment1/Game3001_Assignment1/Assets/_MyAssets/_Scripts/GameManager.cs
+++ Assignment1/Game3001_Assignment1/Assets/_MyAssets/_Scripts/GameManager.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] Camera mainCam;
+    [SerializeField] Camera mainCamera;
     [SerializeField] GameObject characterPrefab;
     [SerializeField] GameObject targetPrefab;
     [SerializeField] GameObject enemyPrefab;
+
+    private GameObject character;
+    private GameObject target;
+    private GameObject enemy;
 
     void Update()
     {
@@ -18,20 +22,74 @@ public class GameManager : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            
+            SpawnAndSetBehavior(SteeringBehaviours.Behavior.Seek);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-
+            SpawnAndSetBehavior(SteeringBehaviours.Behavior.Flee);
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-
+            SpawnAndSetBehavior(SteeringBehaviours.Behavior.Arrive);
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-
+            SpawnAndSetBehavior(SteeringBehaviours.Behavior.Avoid);
         }
     }
 
+    void SpawnAndSetBehavior(SteeringBehaviours.Behavior behavior)
+    {
+        if (character != null)
+        {
+            Destroy(character);
+        }
+
+        DestroyTargets();
+
+        Vector2 randomPosition1 = GetRandomPositionWithinBounds();
+        Vector2 randomPosition2 = GetRandomPositionWithinBounds();
+
+        character = Instantiate(characterPrefab, randomPosition1, Quaternion.identity);
+
+        switch (behavior)
+        {
+            case SteeringBehaviours.Behavior.Seek:
+                target = Instantiate(targetPrefab, randomPosition2, Quaternion.identity);
+                character.GetComponent<SteeringBehaviours>().target = target.transform;
+                break;
+            case SteeringBehaviours.Behavior.Flee:
+                enemy = Instantiate(enemyPrefab, randomPosition2, Quaternion.identity);
+                character.GetComponent<SteeringBehaviours>().enemy = enemy.transform;
+                break;
+            case SteeringBehaviours.Behavior.Arrive:
+                target = Instantiate(targetPrefab, randomPosition2, Quaternion.identity);
+                character.GetComponent<SteeringBehaviours>().target = target.transform;
+                break;
+            case SteeringBehaviours.Behavior.Avoid:
+                enemy = Instantiate(enemyPrefab, randomPosition2, Quaternion.identity);
+                character.GetComponent<SteeringBehaviours>().enemy = enemy.transform;
+                break;
+        }
+
+        character.GetComponent<SteeringBehaviours>().currentBehavior = behavior;
+        character.GetComponent<SteeringBehaviours>().enabled = true;
+    }
+
+    void DestroyTargets()
+    {
+        if (target != null) Destroy(target);
+        if (enemy != null) Destroy(enemy);
+    }
+
+    Vector2 GetRandomPositionWithinBounds()
+    {
+        Vector3 screenBottomLeft = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane));
+        Vector3 screenTopRight = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.nearClipPlane));
+
+        float randomX = Random.Range(screenBottomLeft.x, screenTopRight.x);
+        float randomY = Random.Range(screenBottomLeft.y, screenTopRight.y);
+
+        return new Vector2(randomX, randomY);
+    }
 }
--- Assignment1/Game3001_Assignment1/Assets/_MyAssets/_Scripts/SteeringBehaviours.cs
+++ Assignment1/Game3001_Assignment1/Assets/_MyAssets/_Scripts/SteeringBehaviours.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class SteeringBehaviours : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public enum Behavior { Seek, Flee, Arrive, Avoid }
+    public Behavior currentBehavior;
 
-    // Update is called once per frame
+    public Transform target;
+    public Transform enemy;
+    public float speed = 5f;
+    public float arrivalRadius = 3f;
+    public float avoidDistance = 3f;
+
     void Update()
     {
-        
+        switch (currentBehavior)
+        {
+            case Behavior.Seek:
+                Seek(target);
+                break;
+            case Behavior.Flee:
+                Flee(enemy);
+                break;
+            case Behavior.Arrive:
+                Arrive(target);
+                break;
+            case Behavior.Avoid:
+                Avoid();
+                break;
+        }
+    }
+
+    void Seek(Transform target)
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+        Vector2 newVelocity = direction * speed;
+        transform.position += (Vector3)newVelocity * Time.deltaTime;
+
+        RotateTowards(direction);
+    }
+
+    void Flee(Transform enemy)
+    {
+        Vector2 direction = (transform.position - enemy.position).normalized;
+        Vector2 newVelocity = direction * speed;
+        transform.position += (Vector3)newVelocity * Time.deltaTime;
+
+        RotateTowards(direction);
+    }
+
+    void Arrive(Transform target)
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+        float distance = Vector2.Distance(target.position, transform.position);
+        float desiredSpeed = (distance < arrivalRadius) ? speed * (distance / arrivalRadius) : speed;
+        Vector2 newVelocity = direction * desiredSpeed;
+        transform.position += (Vector3)newVelocity * Time.deltaTime;
+
+        RotateTowards(direction);
+    }
+
+    void Avoid()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, avoidDistance);
+
+        if (hit.collider != null)
+        {
+            Vector2 avoidanceForce = Vector2.Reflect(transform.right, hit.normal) * speed;
+            transform.position += (Vector3)avoidanceForce * Time.deltaTime;
+
+            RotateTowards(avoidanceForce);
+        }
+    }
+
+    void RotateTowards(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
